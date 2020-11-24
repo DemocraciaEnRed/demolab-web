044d57968a474cfe60904a0b8bdfa7303f6a67e9
Guillermo Croppi <guillermocroppi@gmail.com>
2020-11-24 17:58:42 -0300

    Changed

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# somosmuchas-web-nuxt
+# demolab-web
 
 ## Build Setup
 
@@ -18,3 +18,36 @@ $ npm run generate
 ```
 
 For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
+
+---
+
+#### Notes from Guille
+
+Hola,
+
+En /assets/_app.scss podes agregar clases GLOBALES.
+
+Sino, en los componentes `.vue` podes agregar un `<style lang="scss" scoped>  </style>` con tus clases exclusivas para ese componente.
+
+```vue
+<style lang="scss" scoped>
+.my-class{
+  //font-size: 10px;
+}
+</style>
+```
+
+
+
+Luego te voy a dejar escrito mas detalles del repositorio. Pero para que te des una idea: Tecnologias a usar
+
+* VueJS
+* NuxtJS, que es un framework para crear VueJS SSR Applications
+* Bulma CSS Framework
+* Buefy, que es una libreria de componentes VueJS con el estilo de BulmaCSS
+
+Una playlist muy interesante para ver conceptos basicos de VueJS: https://www.youtube.com/playlist?list=PL4cUxeGkcC9gQcYgjhBoeQH7wiAyZNrYa
+
+Esto si es importante que lo veas: La sintaxis de los `.vue` es un Single File Component: https://vuejs.org/v2/guide/single-file-components.html
+
+Si necesitas agregar plugins, avisame.
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'somosmuchas-web-nuxt',
+    title: 'Demolab',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -9,7 +9,9 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css', rel: 'stylesheet', integrity: 'sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==', crossorigin: 'anonymous' }
+      { href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css', rel: 'stylesheet', integrity: 'sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==', crossorigin: 'anonymous' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com'},
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Staatliches&display=swap'}
     ]
   },
 
