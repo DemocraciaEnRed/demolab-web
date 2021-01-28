352c59f98ded5bd12e9a9dfc202c6c7867bb37b3
mijail cohen <mijaelcohen@gmail.com>
2021-01-28 15:15:34 -0300

    custom carrousel indicators

--- components/home/Carrousell.vue
+++ components/home/Carrousell.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-carousel>
+  <b-carousel :indicator-inside="false">
     <b-carousel-item v-for="(carousel, i) in carousels" :key="i">
       <section :class="`hero is-medium is-${carousel.color}`">
         <div class="hero-body has-text-centered">
@@ -9,6 +9,9 @@
         </div>
       </section>
     </b-carousel-item>
+    <template slot="indicators"  slot-scope="props">
+      <span class="custom-item" v-html="carousels[props.i].indicator"/>
+    </template>
   </b-carousel>
 </template>
 
@@ -18,9 +21,9 @@ export default {
   data () {
     return {
       carousels: [
-        { text: '¿Que es DemoLab?', color: 'primary' },
-        { text: 'Evento proximo', color: 'info' },
-        { text: 'Otro evento proximo', color: 'success' }
+        { text: '¿Que es DemoLab?', color: 'primary', indicator: '¿Que es </br> <span>DemoLab?</span>' },
+        { text: 'Evento proximo', color: 'info', indicator: 'Evento </br> <span>proximo</span>' },
+        { text: 'Otro evento proximo', color: 'success', indicator: 'Otro </br> <span>Evento</span>' }
       ]
     }
   }
