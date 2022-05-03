795b1586e74ff13792d026ebbc33b05005252a01
DevHormiga <devhormiga@gmail.com>
2022-05-03 11:56:22 -0300

    prueba de carusel en mobile

--- components/home/Carrousell.vue
+++ components/home/Carrousell.vue
@@ -2,10 +2,17 @@
   <section>
       <b-carousel :indicator-inside="true" :autoplay="true" class="m-0 hero is-medium-mobile">
         <b-carousel-item v-for="(carousel, i) in carousels" :key="i">
-          <section class="m-0">
+          <section v-if="divice" class="m-0">
             <div class="hero-body has-text-centered">
               <a :href="`${carousel.link}`">
                 <img :src="require(`@/assets/${carousel.foto}`)" alt="">
+             </a>
+            </div>
+          </section>
+          <section v-else class="m-0">
+            <div class="hero-body has-text-centered">
+              <a :href="`${carousel.link}`">
+                <img :src="require(`@/assets/${carousel.fotoMobile}`)" alt="">
              </a>
             </div>
           </section>
@@ -20,12 +27,21 @@
 <script>
 export default {
   name: 'Carrousell',
+  beforeMount () {
+    if (window.screen.width > 720) {
+      this.divice = true
+    } else {
+      this.divice = false
+    }
+    console.log('screen: ', this.divice)
+  },
   data () {
     return {
+      divice: true,
       carousels: [
-        { text: '¿Que es DemoLab?', color: 'primary', indicator: '¿Que es </br> <span>DemoLab?</span>', foto: 'demolab.png', link: '/que-es-demolab' },
-        { text: 'Evento proximo', color: 'info', indicator: 'Nuestros </br> <span>Proyectos</span>', foto: 'proyectos.png', link: 'metodologias' },
-        { text: 'Otro evento proximo', color: 'success', indicator: 'Participación </br> <span>Ciudadana</span>', foto: 'participaciondemo.png', link: 'https://participa.demolab.com.co/' }
+        { text: '¿Que es DemoLab?', color: 'primary', indicator: '¿Que es </br> <span>DemoLab?</span>', foto: 'demolab.png', fotoMobile: 'ejemplo.png', link: '/que-es-demolab' },
+        { text: 'Evento proximo', color: 'info', indicator: 'Nuestros </br> <span>Proyectos</span>', foto: 'proyectos.png', fotoMobile: 'ejemplo.png', link: 'metodologias' },
+        { text: 'Otro evento proximo', color: 'success', indicator: 'Participación </br> <span>Ciudadana</span>', foto: 'participaciondemo.png', fotoMobile: 'ejemplo.png', link: 'https://participa.demolab.com.co/' }
       ]
     }
   }
